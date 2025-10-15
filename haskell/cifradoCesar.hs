f3cc380c544de6d98ca07e4f6c9e81df0578ba78
esteban894 <estebanrinaldicampos@gmail.com>
2025-10-15 14:18:15 -0300

    agregado cifrado cesar

---
+++ haskell/cifradoCesar.hs
@@ -0,0 +1,12 @@
+import Data.Char
+
+cifrarChar :: Int -> Char -> Char
+cifrarChar 0 c = c
+cifrarChar n c
+  | isUpper c = chr (mod (ord c - ord 'A' + n) 26 + ord 'A')
+  | isLower c = chr (mod (ord c - ord 'a' + n) 26 + ord 'a')
+  | otherwise = c
+
+cifradoCesar :: Int -> String -> String
+cifradoCesar 0 str = str
+cifradoCesar n str = map (cifrarChar n) str
